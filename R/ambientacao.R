8a9843230eec7b7a7538d37b207c9385b7245dd7
José de Jesus Filho <jjesusfilho@gmail.com>
2020-03-07 11:01:47 -0300

    atualização do projeto

---
+++ R/ambientacao.R
@@ -0,0 +1,11 @@
+
+a <- 2+2
+
+raiz_quadrada <- sqrt(25)
+
+.raiz <- sum(3,4)
+
+url <- "https://gist.githubusercontent.com/jjesusfilho/e9b909014a6e3ed11a3dba7f3bf7254d/raw/1a9fe64e52e4e74c26bc5b492eadf93831b25c9c/direcionamento.json"
+
+df <- jsonlite::fromJSON(url,simplifyDataFrame = T)
+
